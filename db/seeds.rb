be137db6522435b716f1748bbaae0fde98185b4e
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-18 21:32:53 +0100

    add user and fav tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,38 +7,51 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Show.delete_all
-Show.create({
+User.delete_all
+Favourite.delete_all
+
+show1 = Show.create({
   title: "The Great British Bake Off",
   series: 7,
   description: "Master bakers Mary & Paul and the incomparable presenting duo of Mel & Sue are back for another interesting series about baking cakes.",
   image: "placeholder.jpg",
   programmeID: "m013a77"
 })
-Show.create({
+show2 = Show.create({
   title: "Black Sails",
   series: 4,
   description: "Tense and gripping pirate drama",
   image: "pirate.jpg",
   programmeID: "1Sdjis72"
 })
-Show.create({
+show3 = Show.create({
   title: "Wayward pines",
   series: 3,
   description: "Intense psychological drama with an unnerving twist",
   image: "uberMutants.jpg",
   programmeID: "A746lks"
 })
-Show.create({
+show4 = Show.create({
   title: "Misfits",
   series: 5,
   description: "bunch of young offenders get weird powers from an unnatural storm",
   image: "storm.jpg",
   programmeID: "pr187k"
 })
-Show.create({
+show5 = Show.create({
   title: "Red Dwarf",
   series: 2,
   description: "hilarious deep space comedy",
   image: "space.jpg",
   programmeID: "gnjas72"
+})
+user1 = User.create({
+  name: "Brian"
+})
+user2 = User.create({
+  name: "Dotty"
+})
+Favourite.create({
+  user: user1,
+  show: show5
 })
